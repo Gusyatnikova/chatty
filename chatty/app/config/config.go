ad50b47f2a1c1202050706cced5af3096a3373ea
Gusyatnikova <crysnat08@gmail.com>
2022-11-01 09:21:08 +0300

    feat(config): delete config.yml

--- chatty/app/config/config.go
+++ chatty/app/config/config.go
@@ -13,8 +13,8 @@ type (
 	}
 
 	HTTP struct {
-		Port string `env-required:"true" yaml:"port" env:"HTTP_PORT"`
-		Host string `env-required:"true" yaml:"host" env:"HTTP_HOST"`
+		Port string `env-required:"true" env:"HTTP_PORT"`
+		Host string `env-required:"true" env:"HTTP_HOST"`
 	}
 
 	Log struct {
@@ -22,9 +22,9 @@ type (
 	}
 
 	PG struct {
-		PoolMax  int    `env-required:"true" yaml:"pool_max" env:"PG_POOL_MAX"`
-		Host     string `env-required:"true" yaml:"host" env:"PG_HOST"`
-		Port     int    `env-required:"true" yaml:"port" env:"PG_PORT"`
+		PoolMax  int    `env-required:"true" env:"PG_POOL_MAX"`
+		Host     string `env-required:"true" env:"PG_HOST"`
+		Port     int    `env-required:"true" env:"PG_PORT"`
 		User     string `env-required:"true" env:"PG_USER"`
 		Password string `env-required:"true" env:"PG_PASSWORD"`
 		DbName   string `env-required:"true" env:"PG_DB_NAME"`
@@ -36,12 +36,7 @@ type (
 func NewConfig() (*Config, error) {
 	cfg := &Config{}
 
-	err := cleanenv.ReadConfig("./chatty/app/config/config.yml", cfg)
-	if err != nil {
-		return nil, fmt.Errorf("err in config.NewConfing.ReadConfig(): %w", err)
-	}
-
-	err = cleanenv.ReadEnv(cfg)
+	err := cleanenv.ReadEnv(cfg)
 	if err != nil {
 		return nil, fmt.Errorf("err in config.NewConfing.ReadEnv(): %w", err)
 	}
