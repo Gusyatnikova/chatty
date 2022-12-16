01ade4de3fb43e6c48093e317f43700eb0a06f5f
Gusyatnikova@github.com <crysnat08@gmail.com>
2022-12-16 15:52:19 +0300

    feat(core): added getUser http handler

---
+++ chatty/delivery/http/http_v1/user.go
@@ -0,0 +1,18 @@
+package http_v1
+
+import (
+	"chatty/chatty/entity"
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func (e *ServerHandler) GetUser(eCtx echo.Context) error {
+	userLogin := entity.UserLogin(eCtx.Param("login"))
+
+	user, err := e.uc.GetUserByLogin(eCtx.Request().Context(), userLogin)
+	if err != nil {
+		return err
+	}
+
+	return eCtx.JSON(http.StatusOK, userToRegisterRespBody(user))
+}
