7f695c1a94bc722f7b4c155faf61eb3c21a07544
Gusyatnikova@github.com <crysnat08@gmail.com>
2023-01-03 11:44:13 +0300

    refactor(core): refactor delivery errors

--- chatty/delivery/errors.go
+++ chatty/delivery/errors.go
@@ -2,6 +2,8 @@ package delivery
 
 import "github.com/pkg/errors"
 
-var ErrBadContentType = errors.New("Content-Type application/json is missing")
-var ErrBadRequestBody = errors.New("Request body is incorrect")
-var ErrUnauthorizied = errors.New("Authorization is required")
+var (
+	ErrBadContentType = errors.New("Content-Type application/json is missing")
+	ErrBadRequestBody = errors.New("Request body is incorrect")
+	ErrUnauthorizied  = errors.New("Authorization is required")
+)
