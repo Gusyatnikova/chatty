f5b5076f0d2026fe8b2093c16385b6c7e6531665
Gusyatnikova@github.com <crysnat08@gmail.com>
2023-01-03 20:47:40 +0300

    refactor(core): refactor errors value

--- pkg/password/errors.go
+++ pkg/password/errors.go
@@ -2,5 +2,5 @@ package password
 
 import "github.com/pkg/errors"
 
-var ErrInvalidHash = errors.New("The encoded hash is not in the correct format")
-var ErrIncompatibleVersion = errors.New("Incompatible version of argon2")
+var ErrInvalidHash = errors.New("the encoded hash is not in the correct format")
+var ErrIncompatibleVersion = errors.New("incompatible version of argon2")
