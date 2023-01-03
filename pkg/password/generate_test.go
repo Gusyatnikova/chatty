c29bb71aedc5a6066b4e6088845a179d294db841
Gusyatnikova@github.com <crysnat08@gmail.com>
2023-01-03 20:51:20 +0300

    refactor(test): refactor TestService_Generate

--- pkg/password/generate_test.go
+++ pkg/password/generate_test.go
@@ -7,20 +7,6 @@ import (
 )
 
 func TestService_Generate(t *testing.T) {
-	tests := []struct {
-		name       string
-		password   string
-		wantLength int
-		wantErr    bool
-	}{
-		{
-			name:       "Should return particular length string with nil error for non-empty password",
-			password:   "qwerty123",
-			wantLength: 101,
-			wantErr:    false,
-		},
-	}
-
 	service := NewService(config.Password{
 		Secret:      "test!@#123",
 		Memory:      1024,
@@ -30,14 +16,20 @@ func TestService_Generate(t *testing.T) {
 		Parallelism: 2,
 	})
 
-	for _, tt := range tests {
-		convey.Convey(tt.name, t, func() {
-			{
-				got, err := service.Generate(tt.password)
+	convey.Convey("Should return particular length string with nil error for non-empty password", t, func() {
+		password, wantLength := "qwerty123", 132
 
-				convey.So(err != nil, convey.ShouldEqual, tt.wantErr)
-				convey.So(got, convey.ShouldHaveLength, tt.wantLength)
-			}
+		got, err := service.Generate(password)
+
+		convey.So(err, convey.ShouldBeNil)
+		convey.So(got, convey.ShouldHaveLength, wantLength)
+
+		convey.Convey("Should return a different string of the same length for the same password", func() {
+			gotRegen, err := service.Generate(password)
+
+			convey.So(err, convey.ShouldBeNil)
+			convey.So(gotRegen, convey.ShouldHaveLength, wantLength)
+			convey.So(gotRegen, convey.ShouldNotEqual, got)
 		})
-	}
+	})
 }
