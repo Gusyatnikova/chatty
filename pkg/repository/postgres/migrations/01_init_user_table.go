c40a131d34857d255452a371725c0856ce26648e
Gusyatnikova@github.com <crysnat08@gmail.com>
2023-03-05 21:12:04 +0300

    refactor(core): changed user table definition

--- pkg/repository/postgres/migrations/01_init_user_table.go
+++ pkg/repository/postgres/migrations/01_init_user_table.go
@@ -8,7 +8,7 @@ import (
 const (
 	InitUserTableSqlQry = `create table if not exists public.user
 				(
-					id           uuid                        primary key,
+					id           varchar                        primary key,
 					login        varchar                     not null unique,
 					password     varchar                     not null,
 					email        varchar                     not null unique,
