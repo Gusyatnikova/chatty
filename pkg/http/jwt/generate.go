557be55fedb0ce6f6529a55b3d6e10189a20b80f
Gusyatnikova@github.com <crysnat08@gmail.com>
2023-03-05 15:25:20 +0300

    refactor(core): in jwt pkg rename GenerateJwtToken -> Generate

--- pkg/http/jwt/generate.go
+++ pkg/http/jwt/generate.go
@@ -7,7 +7,7 @@ import (
 	"github.com/golang-jwt/jwt"
 )
 
-func (e *JWTManager) GenerateJwtToken(user entity.User) (string, error) {
+func (e *JWTManager) Generate(user entity.User) (string, error) {
 	token := jwt.NewWithClaims(SigningMethod, jwt.StandardClaims{
 		ExpiresAt: time.Now().Add(time.Second * time.Duration(e.cfg.TTL)).Unix(),
 		Subject:   string(user.GetID()),
