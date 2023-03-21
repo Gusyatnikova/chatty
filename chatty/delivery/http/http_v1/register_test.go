1bc597850ac5ff3d5e92869761ad04515ebd1a9d
Gusyatnikova@github.com <crysnat08@gmail.com>
2023-03-21 22:39:51 +0300

    feat(tests): add test Register http handler

---
+++ chatty/delivery/http/http_v1/register_test.go
@@ -0,0 +1,163 @@
+package http_v1
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+
+	"chatty/chatty/delivery"
+	"chatty/chatty/entity"
+	"chatty/chatty/usecase"
+	ucmocks "chatty/chatty/usecase/mocks"
+	"chatty/pkg/http/jwt"
+	jwtmocks "chatty/pkg/http/jwt/mocks"
+)
+
+func TestRegister(t *testing.T) {
+	testCases := []struct {
+		name         string
+		body         map[string]interface{}
+		mockUC       bool
+		mockJWT      bool
+		expectedErr  error
+		expectedUser entity.User
+		errFromUC    error
+	}{
+		{
+			name:        "ok, successful registration",
+			expectedErr: nil,
+			errFromUC:   nil,
+			mockUC:      true,
+			mockJWT:     true,
+			expectedUser: entity.User{
+				Creds: entity.UserCreds{
+					Login:    "TestLogin",
+					Password: "!@@@key123",
+				},
+				Contacts: entity.UserContacts{
+					Email:       "test22@gmail.com",
+					PhoneNumber: "+713333333333",
+				},
+			},
+			body: map[string]interface{}{
+				"login":        "TestLogin",
+				"password":     "!@@@key123",
+				"email":        "test22@gmail.com",
+				"phone_number": "+713333333333",
+			},
+		}, {
+			name:         "nok, invalid request body: wrong type",
+			expectedErr:  delivery.ErrBadRequestBody,
+			errFromUC:    nil,
+			expectedUser: entity.User{},
+			body: map[string]interface{}{
+				"login": 12345,
+			},
+		}, {
+			name:         "nok, get error from usecase: user duplication",
+			expectedErr:  usecase.ErrUserDuplication,
+			errFromUC:    usecase.ErrUserDuplication,
+			mockUC:       true,
+			expectedUser: entity.User{},
+			body: map[string]interface{}{
+				"login":        "TestLogin",
+				"password":     "!@@@key123",
+				"email":        "test22@gmail.com",
+				"phone_number": "+713333333333",
+			},
+		}, {
+			name:         "nok, get error from usecase: user not found for response",
+			expectedErr:  usecase.ErrNoUser,
+			errFromUC:    usecase.ErrNoUser,
+			mockUC:       true,
+			expectedUser: entity.User{},
+			body: map[string]interface{}{
+				"login":        "TestLogin",
+				"password":     "!@@@key123",
+				"email":        "test22@gmail.com",
+				"phone_number": "+713333333333",
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			body, _ := json.Marshal(tc.body)
+			req := httptest.NewRequest(http.MethodPost, "/register", bytes.NewReader(body))
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+			rec := httptest.NewRecorder()
+
+			eCtx := getEchoContext(req, rec)
+			h := newRegisterHandler(prepareMocks(
+				t, eCtx.Request().Context(), tc.mockUC, tc.mockJWT, tc.expectedUser, tc.errFromUC))
+
+			err := h.Register(eCtx)
+
+			if err != nil {
+				assert.True(t, errors.Is(err, tc.expectedErr))
+				return
+			}
+
+			assert.Equal(t, userToRespBody(tc.expectedUser), actualRespBody(rec))
+		})
+	}
+}
+
+func prepareMocks(t *testing.T, ctx context.Context, mockUC bool, mockJWT bool, user entity.User, ucErr error) (
+	muc usecase.ChatUseCase, mjwt jwt.TokenManager) {
+	if !mockUC && !mockJWT {
+		return nil, nil
+	}
+
+	if mockUC {
+		uc := ucmocks.NewChatUseCase(t)
+		uc.On("Register", ctx, mock.Anything, mock.Anything).Return(ucErr)
+
+		if ucErr == nil {
+			uc.On("GetUserByLogin", ctx, mock.Anything).Return(user, nil)
+		}
+
+		muc = uc
+	}
+
+	if mockJWT {
+		jwtManager := jwtmocks.NewTokenManager(t)
+		jwtManager.On("GenerateAccessToken", mock.Anything).Return("", time.Now(), nil)
+		mjwt = jwtManager
+	}
+
+	return
+}
+
+func newRegisterHandler(uc usecase.ChatUseCase, jwtManager jwt.TokenManager) *ServerHandler {
+	return &ServerHandler{
+		uc:         uc,
+		jwtManager: jwtManager,
+	}
+}
+
+func actualRespBody(r *httptest.ResponseRecorder) userRespBody {
+	rBody := userRespBody{}
+	json.NewDecoder(r.Body).Decode(&rBody)
+
+	return rBody
+}
+
+func getEchoContext(r *http.Request, w http.ResponseWriter) echo.Context {
+	e := echo.New()
+
+	eCtx := e.NewContext(r, w)
+	eCtx.SetPath(r.URL.Path)
+
+	return eCtx
+}
